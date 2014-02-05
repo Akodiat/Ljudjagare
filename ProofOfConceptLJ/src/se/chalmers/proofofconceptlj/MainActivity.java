4db9ed66ea5e922dff259ed9b19ce48c69f26abf
Mushu <linus@mushu.it>
2014-02-05 15:57:57 +0100

    Tested and successfully implemented panning of the sound.
    Note that the test sound being used is a stereo signal. The effect will
    sound better with a mono source.

--- ProofOfConceptLJ/src/se/chalmers/proofofconceptlj/MainActivity.java
+++ ProofOfConceptLJ/src/se/chalmers/proofofconceptlj/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.*;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity {
 	private static final int FX_01 = 1;
@@ -22,7 +23,13 @@ public class MainActivity extends Activity {
 
 	private Button playFX;
 	private Button sweepFX;
+	private Button stopLoop;
 	private SeekBar seekBar;
+
+	private double panning;
+	private int id;
+	
+	private boolean stopAudio = false;
 
 	/**
 	 * True if sound is loaded correctly.
@@ -41,10 +48,11 @@ public class MainActivity extends Activity {
 		playFX.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				playFX(FX_01);
+				stopAudio = false;
+				id = soundPool.play(FX_01, 1f, 1f, 1, LOOP, 1f);
 			}
 		});
-		
+
 		sweepFX = (Button) findViewById(R.id.sweep_fx1);
 		sweepFX.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -55,6 +63,36 @@ public class MainActivity extends Activity {
 					// Need to catch eventual exception thrown by testPanning.
 					e.printStackTrace();
 				}
+			}
+		});
+		
+		stopLoop = (Button) findViewById(R.id.stop_loop);
+		stopLoop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				soundPool.stop(id);
+			}
+		});
+
+		seekBar = (SeekBar) findViewById(R.id.seekBar1);
+		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				panning = Math.PI / 2 * (progress) / 100;
+				float leftVolume = (float) Math.cos(panning);
+				float rightVolume = (float) Math.sin(panning);
+				soundPool.setVolume(id, leftVolume, rightVolume);
 			}
 		});
 	}
@@ -82,7 +120,7 @@ public class MainActivity extends Activity {
 		});
 
 		// Load FX
-		soundPoolMap.put(FX_01, soundPool.load(this, R.raw.percussion, 1));
+		soundPoolMap.put(FX_01, soundPool.load(this, R.raw.sound, 1));
 	}
 
 	// public void playAtCoordinate(Vector2 coord, Human human) {
@@ -108,22 +146,36 @@ public class MainActivity extends Activity {
 		// Subtract 100 to get a seekbar from -100 to 100 (max is 200).
 		// int seekValue = seekBar.getProgress() - 100;
 
-//		float actualVolume = (float) audioManager
-//				.getStreamVolume(AudioManager.STREAM_MUSIC);
-//		float maxVolume = (float) audioManager
-//				.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-//		float volume = actualVolume / maxVolume;
+		// float actualVolume = (float) audioManager
+		// .getStreamVolume(AudioManager.STREAM_MUSIC);
+		// float maxVolume = (float) audioManager
+		// .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		// float volume = actualVolume / maxVolume;
 
 		// Play sound
 		if (loaded)
 			soundPool.play(FX_01, 1f, 1f, 1, 0, 1f);
 	}
 
+	public void setPanning() {
+		// 0 - all left, pi/2 all right
+		double radian = panning;
+		float leftVolume = (float) Math.cos(radian);
+		float rightVolume = (float) Math.sin(radian);
+
+		soundPool.play(FX_01, leftVolume, rightVolume, 1, 0, 1f);
+	}
+
+	public void loopForever() {
+
+	}
+
 	/**
 	 * Take from http://alvinalexander.com/java/
 	 * jwarehouse/android/media/tests/SoundPoolTest/
 	 * src/com/android/SoundPoolTest.java.shtml
-	 * @throws InterruptedException 
+	 * 
+	 * @throws InterruptedException
 	 */
 	public void testPanning(int soundID) throws InterruptedException {
 		int id = soundPool.play(soundID, 0f, 1f, 1, LOOP, 1f);
@@ -135,7 +187,7 @@ public class MainActivity extends Activity {
 			float rightVolume = (float) Math.cos(radians);
 			soundPool.setVolume(id, leftVolume, rightVolume);
 		}
-		
+
 		soundPool.stop(id);
 	}
 }
