8b4f92069b973c5389372abab98190ec7e4d34b6
Akodiat <joajohan@student.chalmers.se>
2014-02-11 08:47:21 +0100

    Integrated Linus sound implementation to some extent

---
+++ ProofOfConceptLJ/src/se/chalmers/proofofconceptlj/FXHandler.java
@@ -0,0 +1,129 @@
+package se.chalmers.proofofconceptlj;
+
+import java.util.HashMap;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+
+public class FXHandler {
+	public static final int FX_01 = 1;
+	public static final int LOOP = -1;
+	
+	private static final int maxAudiableDistance = 50; //Meters
+
+	private HashMap<Integer, Integer> soundPoolMap;
+	private SoundPool soundPool;
+
+	private AudioManager am;
+
+	private double panning;
+	
+
+	// True if sound is loaded correctly
+	private boolean loaded = false;
+
+	/**
+	 * Initialize sound engine
+	 */
+	@SuppressLint("UseSparseArrays")
+	public void initSound(Context context) {
+		am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+		soundPoolMap = new HashMap<Integer, Integer>();
+		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+			@Override
+			public void onLoadComplete(SoundPool soundPool, int sampleId,
+					int status) {
+				loaded = true;
+			}
+		});
+
+		// Load FX
+		soundPoolMap.put(FX_01, soundPool.load(context, R.raw.sound, 1));
+	}
+
+	/**
+	 * Play a sound one without any panning.
+	 * 
+	 * @param soundID
+	 * @param times
+	 * @return the streamID if successful, non-zero value if not.
+	 */
+	public int playFX(int soundID, int times) {
+		if (loaded)
+			return soundPool.play(soundID, 1f, 1f, 1, 0, 1f);
+		return -42;
+	}
+
+	public int playFX(int soundID) {
+		if (loaded)
+			return soundPool.play(soundID, 1f, 1f, 1, -1, 1f);
+		return -42;
+	}
+	
+	public void stopFX(int soundID) {
+		if (loaded)
+			soundPool.stop(soundID);
+	}
+
+	/**
+	 * 
+	 * @param soundID sound to process
+	 * @param angle angle between current direction and source (0-360)
+	 */
+	public void setPanning(int soundID, float angle, float distance) {
+		float distFactor = (float) ((Math.pow(-distance, 2) / Math.pow(maxAudiableDistance, 2)) + 1);
+		if (distFactor<0)
+			distFactor = 0;
+
+		// Is sound behind the player to the left?
+		if (angle >= 90 && angle < 180){
+			soundPool.setVolume(soundID, 1f*distFactor, 0);
+			return;
+		}
+
+		// Is sound behind the player to the right?
+		if (angle >= 180){
+			soundPool.setVolume(soundID, 0, 1f*distFactor);
+			return;
+		}
+
+		// 0 - all left, pi/2 all right
+		double radian = angle * (Math.PI / 180);											//= panning;
+		float leftVolume = (float) Math.cos(radian) * distFactor;
+		float rightVolume = (float) Math.sin(radian)* distFactor;
+
+		soundPool.setVolume(soundID, leftVolume, rightVolume);
+	}
+
+	/**
+	 * Sweeping sound from left to right
+	 */
+	public void sweepFX(int soundID) throws InterruptedException {
+		int id = soundPool.play(soundID, 0f, 1f, 1, LOOP, 1f);
+
+		for (int count = 0; count < 101; count++) {
+			Thread.sleep(20);
+			double radians = (Math.PI / 2) * count / 100;
+
+			soundPool.setVolume(id, (float) Math.cos(radians),
+					(float) Math.sin(radians));
+		}
+
+		soundPool.stop(id);
+	}
+
+	/** 
+	 * Get max value of device
+	 */
+	public int maxVolume() {
+		return am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+	}
+
+	/**
+	 * 
+	 */
+}
--- ProofOfConceptLJ/src/se/chalmers/proofofconceptlj/MainActivity.java
+++ ProofOfConceptLJ/src/se/chalmers/proofofconceptlj/MainActivity.java
@@ -16,16 +16,20 @@ public class MainActivity extends Activity {
 	final static String TAG = "PAAR";
 	SensorManager sensorManager;
 	ImageView arrow2;
-		int orientationSensor;
-		float headingAngle;
-		float pitchAngle;
-		float rollAngle;
+	int orientationSensor;
+	float headingAngle;
+	float pitchAngle;
+	float rollAngle;
 
 	LocationManager locationManager;
 
 
 	Location source;
 	Human human;
+
+	// Handles all form of audio
+	FXHandler fx;
+	int streamID;
 
 
 
@@ -34,17 +38,22 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//Stolen from augmented reality on the Android platform pp.23:
-				sensorManager 		= (SensorManager) getSystemService(SENSOR_SERVICE);
-				orientationSensor 	= Sensor.TYPE_ORIENTATION;
-				sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager 		= (SensorManager) getSystemService(SENSOR_SERVICE);
+		orientationSensor 	= Sensor.TYPE_ORIENTATION;
+		sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
 
 		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 2, locationListener);
 		human = new Human(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
 
 		arrow2 = (ImageView) this.findViewById(R.id.imageView2);
+		
+		streamID = -1;
+
+		// Initialize audio
+		(fx = new FXHandler()).initSound(this);
 	}
-		final SensorEventListener sensorEventListener = new SensorEventListener() {
+	final SensorEventListener sensorEventListener = new SensorEventListener() {
 		public void onSensorChanged(SensorEvent sensorEvent) {
 			if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
 				float prevHeading = headingAngle; //Store the old value to rotate arrow
@@ -52,9 +61,9 @@ public class MainActivity extends Activity {
 				headingAngle 	= sensorEvent.values[0];
 				pitchAngle 		= sensorEvent.values[1];
 				rollAngle 		= sensorEvent.values[2];
-//				Log.d(TAG, "Heading: " + String.valueOf(headingAngle));
-//				Log.d(TAG, "Pitch: " + String.valueOf(pitchAngle));
-//				Log.d(TAG, "Roll: " + String.valueOf(rollAngle));
+				//				Log.d(TAG, "Heading: " + String.valueOf(headingAngle));
+				//				Log.d(TAG, "Pitch: " + String.valueOf(pitchAngle));
+				//				Log.d(TAG, "Roll: " + String.valueOf(rollAngle));
 
 
 				//message("Pitch: " + String.valueOf(pitchAngle));
@@ -63,11 +72,14 @@ public class MainActivity extends Activity {
 						"\nPitch: " + String.valueOf(pitchAngle) + 
 						"\nRoll: " + String.valueOf(rollAngle));
 
-//				if(usingCompass()) {
-//					human.setRotation(headingAngle);
-//				}
-				if(source != null)
+				//				if(usingCompass()) {
+				//					human.setRotation(headingAngle);
+				//				}
+				if(source != null){
 					pointArrowToSource_C();
+					if(streamID != -1)
+						fx.setPanning(streamID, (headingAngle + human.getLocation().bearingTo(source)), human.getLocation().distanceTo(source));
+				}
 			}
 		}
 		public void onAccuracyChanged (Sensor senor, int accuracy) {
@@ -111,12 +123,12 @@ public class MainActivity extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		//		sensorManager.registerListener(sensorEventListener, sensorManager
-		//				.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(sensorEventListener, sensorManager
+				.getDefaultSensor(orientationSensor), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 	@Override
 	public void onPause() {
-		//		sensorManager.unregisterListener(sensorEventListener);
+		sensorManager.unregisterListener(sensorEventListener);
 		super.onPause();
 	}
 
@@ -129,67 +141,46 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	/**
+	 * Points arrow to source using GPS bearing
+	 */
 	private void pointArrowToSource_G() {
 		ImageView arrow = (ImageView) this.findViewById(R.id.imageView3);
 		arrow.setRotation(human.getLocation().getBearing() + human.getLocation().bearingTo(source));
 	}
+
+	/**
+	 * Points arrow to source using compass orientation
+	 */
 	private void pointArrowToSource_C() {
 		ImageView arrow = (ImageView) this.findViewById(R.id.imageView2);
 		arrow.setRotation(headingAngle + human.getLocation().bearingTo(source));
 	}
+
 	private boolean usingCompass() {
 		CheckBox checkBox = (CheckBox) this.findViewById(R.id.checkBox_compass);
 		return checkBox.isChecked();
 	}
-	public void playAtCoordinate() {
-		/*		//Initiate mediaPlayer with dragon roar ^^
-		MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.dragon);
-
-		double 	someConstant  		= 0.5;	//  <-- Edit this (has to be between 0 and 1)
-
-		//Volume for each ear is inverse proportional to the distance to the sound source
-		float left  = (float) (1 / (Vector2.distance(coord, human.getLeftEarPos())  * someConstant));
-		float right = (float) (1 / (Vector2.distance(coord, human.getRightEarPos()) * someConstant));
-
-
-		mediaPlayer.setVolume(left, right); //TODO: has to be in interval (0 <= left&right <= 1)
-
-
-		//mediaPlayer.setLooping(true);
-		mediaPlayer.start(); // no need to call prepare(); create() does that for you
-
-		printOrientation("L:"+ left + "\tR:" + right);
-
-
-
-
-
-		while(mediaPlayer.isPlaying()); //Stops thread until done playing (fulhack)
-
-		mediaPlayer.release();
-		 */
-
-	}
 
 	public void playSound(View view) {
-		//Spela ljudet här linus
+		if(this.streamID == -1) {
+			this.streamID = fx.playFX(FXHandler.FX_01);
+		}
+		else {
+			fx.stopFX(streamID);
+			this.streamID = -1;
+		}
 	}
 
 	public void setCurrentAsSource(View view) {
-		if(this.human != null) {
-			this.source = this.human.getLocation();
+		this.source = this.human.getLocation();
 
-			TextView textLongitude = (TextView) this.findViewById(R.id.textView_sourceLongitude);
-			TextView textLatitude = (TextView) this.findViewById(R.id.textView_sourceLatitude);
+		TextView textLongitude = (TextView) this.findViewById(R.id.textView_sourceLongitude);
+		TextView textLatitude = (TextView) this.findViewById(R.id.textView_sourceLatitude);
 
-			textLongitude.setText("Source longitude: " + this.source.getLongitude());
-			textLatitude.setText(  "Source latitude: " + this.source.getLatitude());
-		}
-		else {
-			TextView textLongitude = (TextView) this.findViewById(R.id.textView_sourceLongitude);
-			textLongitude.setText("Location not found!");
-		}
-
+		textLongitude.setText("Source longitude: " + this.source.getLongitude());
+		textLatitude.setText(  "Source latitude: " + this.source.getLatitude());
 	}
 
 	private void printOrientation(String s) {
