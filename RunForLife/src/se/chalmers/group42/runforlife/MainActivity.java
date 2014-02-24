9f315c9ecb40026bc1b9690f6cb98c11537bf722
epoxy <palmqvist.anton@gmail.com>
2014-02-24 21:55:20 +0100

    fancy cover flow working

--- RunForLife/src/se/chalmers/group42/runforlife/MainActivity.java
+++ RunForLife/src/se/chalmers/group42/runforlife/MainActivity.java
@@ -3,13 +3,29 @@ package se.chalmers.group42.runforlife;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import at.technikum.mti.fancycoverflow.FancyCoverFlow;
+import at.technikum.mti.fancycoverflow.FancyCoverFlowSampleAdapter;
 
 public class MainActivity extends Activity {
 
+	private FancyCoverFlow fancyCoverFlow;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		fancyCoverFlow = (FancyCoverFlow) this.findViewById(R.id.fancyCoverFlow);
+		
+		fancyCoverFlow.setAdapter(new FancyCoverFlowSampleAdapter());
+        fancyCoverFlow.setUnselectedAlpha(1.0f);
+        fancyCoverFlow.setUnselectedSaturation(0.0f);
+        fancyCoverFlow.setUnselectedScale(0.5f);
+        fancyCoverFlow.setSpacing(50);
+        fancyCoverFlow.setMaxRotation(0);
+        fancyCoverFlow.setScaleDownGravity(0.2f);
+        fancyCoverFlow.setActionDistance(FancyCoverFlow.ACTION_DISTANCE_AUTO);
+		
 	}
 
 	@Override
