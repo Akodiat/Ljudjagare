9b42fb98cda35b452f71b236847466b2970e45c8
Linus Karlsson <linus@mushu.it>
2014-03-05 15:32:45 +0100

    ShooterActivity should now be able to launch

--- RunForLife/src/se/chalmers/group42/gameModes/ShooterActivity.java
+++ RunForLife/src/se/chalmers/group42/gameModes/ShooterActivity.java
@@ -1,5 +1,21 @@
 package se.chalmers.group42.gameModes;
 
-public class ShooterActivity {
+import se.chalmers.group42.runforlife.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
+public class ShooterActivity extends Activity {
+
+	private TextView monsterNotification;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		monsterNotification = (TextView) findViewById(R.id.monster_notification);
+	}
+
+	public void monsterSpotted() {
+		monsterNotification.setText("RAAWR");
+	}
 }
