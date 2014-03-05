2777282f41bcd150546772decc709dae01957d7e
Akodiat <joakimjo@swipnet.se>
2014-03-05 11:24:52 +0100

    Added documentation and a Location to LatLng method to LocationHelper

--- RunForLife/src/utils/LocationHelper.java
+++ RunForLife/src/utils/LocationHelper.java
@@ -1,15 +1,33 @@
 package utils;
 
 import android.location.Location;
-
 import com.google.android.gms.maps.model.LatLng;
 
+/**
+ * A help class for converting between different position formats
+ * @author Joakim Johansson
+ */
 public class LocationHelper {
-	
+	/**
+	 * Converts a LatLng object into a Location
+	 * @param latLng the LatLng object describing a position
+	 * @return A location describing the same position as latLng
+	 */
 	public static Location locationFromLatlng(LatLng latLng) {
 		Location location = new Location("Trololo");
 		location.setLatitude(latLng.latitude);
 		location.setLongitude(latLng.longitude);
 		return location;
+	}
+	/**
+	 * Converts a Location object into a LatLng
+	 * @param location the Location object describing a position
+	 * @return A LatLng describing the same position as location
+	 */
+	public static LatLng latlngFromLocation(Location location) {
+		return new LatLng(
+				location.getLatitude(), 
+				location.getLongitude()
+		);
 	}
 }
