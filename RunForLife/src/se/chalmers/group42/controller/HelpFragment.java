02babafa634563a06604af8b5ba85006d7f887be
epoxy <palmqvist.anton@gmail.com>
2014-05-03 14:14:54 +0200

    hundread meter sound added

--- RunForLife/src/se/chalmers/group42/controller/HelpFragment.java
+++ RunForLife/src/se/chalmers/group42/controller/HelpFragment.java
@@ -5,6 +5,7 @@ import java.util.TimerTask;
 
 import se.chalmers.group42.runforlife.FXHandler;
 import se.chalmers.group42.runforlife.R;
+import se.chalmers.group42.runforlife.Speech;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,13 +37,13 @@ public class HelpFragment extends Fragment{
 		//Setting up
 		correctDirectionButton = (Button) rootView.findViewById(R.id.button1);
 		hundredMetersButton = (Button) rootView.findViewById(R.id.button2);
+		frequencySeekbar = (SeekBar) rootView.findViewById(R.id.seekBar1);
 		frequencyButton = (Button) rootView.findViewById(R.id.button3);
 		leftButton = (Button) rootView.findViewById(R.id.buttonLeft);
 		rightButton = (Button) rootView.findViewById(R.id.buttonRight);
 		wrongDirectionButton = (Button) rootView.findViewById(R.id.buttonBehind);
 		coinPickButton = (Button) rootView.findViewById(R.id.button4);
 		newCoinButton = (Button) rootView.findViewById(R.id.button5);
-		frequencySeekbar = (SeekBar) rootView.findViewById(R.id.seekBar1);
 		timer = new Timer();
 		
 		//Button actions
@@ -61,6 +62,15 @@ public class HelpFragment extends Fragment{
 		});
 		
 		//Hundred meters sound
+		hundredMetersButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				fxHandler.sayDistance(new Speech(2));
+			}
+		});
+		
+		
+		
 		
 
 		return rootView;
