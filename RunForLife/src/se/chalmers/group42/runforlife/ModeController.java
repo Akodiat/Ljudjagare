823ed1d339913fccfcc6f7ef238a327493b1c0be
Akodiat <joakimjo@swipnet.se>
2014-05-05 11:09:30 +0200

    Readded CoinCollector activity

--- RunForLife/src/se/chalmers/group42/runforlife/ModeController.java
+++ RunForLife/src/se/chalmers/group42/runforlife/ModeController.java
@@ -45,7 +45,7 @@ public class ModeController {
 	 * @return 
 	 */
 	private void launchCoinCollector() {
-		Intent runActivityIntent = new Intent(context, FreerunActivity.class);
+		Intent runActivityIntent = new Intent(context, CoinCollectorActivity.class);
 		context.startActivity(runActivityIntent);
 	}
 }
